b09c15fe7a60ef4a26eb77291b3550f8518385eb
Corbi <allan.nicolas331966@gmail.com>
2025-06-26 10:41:07 +1100

    ajout des conversation dans l'intro

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import time
 from fonctions.display_scene import display_scene
 from fonctions.choix import choix
+from story.intro import scene_intro, choix_intro, rep_intro
 
-# Texte de la scène (par paragraphe)
-scene = [
-    "??? - WEIMIN!!! FAUT QU'ON PARLE!!!\n",
-    "vous vous reveillez par les cris de votre mère qui vous appelle.",
-    "vous êtes dans votre chambre, allongé sur votre lit.",
-    "Vous vous levez et vous allez voir votre mère.\n",
-    "el mama - il est 14h30, tu a dormi toute la matinée et tu na toujour pas de travail.\n",
-    "Vous descendez les escaliers.",
-    "Dans la cuisine, vous voyez votre mère, accompagnée de votre père.\n",
-    "CHOIX"
-]
-
-display_scene()
+display_scene(scene_intro)
+choix(choix_intro, rep_intro)
 time.sleep(1)
-print("Vous avez jusqu'à ce soir pour trouver du travail, sinon vous devrez quitter la maison de vos parents.") 
+display_scene(["Vous avez jusqu'à ce soir pour trouver du travail, sinon vous devrez quitter la maison de vos parents."])
