779e6a24968bd33bf7c073461f33451f89e377aa
BOUILLE Manley <manley-bouille@outlook.fr>
2025-06-26 10:45:54 +1100

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-'''
-??? - WEIMIN!!! FAUT QU'ON PARLE!!!
+# main.py
+# This is the main entry point for the game.
+import sys
+import os
 
-vous vous reveillez par les cris de votre mère qui vous appelle. 
-vous êtes dans votre chambre, allongé sur votre lit. 
-Vous vous levez et vous allez voir votre mère.
+# Importing necessary modules
+from Fonctions.choix import choix
+from Fonctions.display_scene import display_scene
 
-el mama - il est 14h30, tu a dormi toute la matinée et tu na toujour pas de travail.
+def main():
+    display_scene(scene)
+    choix("Je vais chercher du travail.", "Je reste ici.", "Bonne chance!", "Tu devrais vraiment chercher un emploi.")
 
-Vous descendez les escaliers et vous voyez votre mère dans la cuisine avec votre père a coté d'elle.
 
-rep 1- oui maman, tu me répétes toujour la même chose, je vais trouver du travail.
-rep 2- sa va sa va
 
-maman rep 1- tu nous dit sa tout les jours a ton père et a moi, mais tu ne fais rien pour trouver du travail.
-
-maman rep 2- non sa ne va pas dutout.
-
-maman - si tu ne trouves pas de travail a partir de ce soir, tu vas devoir partir de la maison.
-
-vous sortez de la maison en quéte de travail.
-vous vous retrouvez dans la rue, vous avez jusqu'à ce soir pour trouver du travail, sinon vous devrez quitter la maison de vos parents.
-'''
-
-import time
-from fonctions.display_scene import display_scene
-from fonctions.choix import choix
-
-# Texte de la scène (par paragraphe)
 scene = [
     "??? - WEIMIN!!! FAUT QU'ON PARLE!!!\n",
     "vous vous reveillez par les cris de votre mère qui vous appelle.",
