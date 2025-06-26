d653ded16beb748b35da1fe3ab8dc624e89e0e5b
BOUILLE Manley <manley-bouille@outlook.fr>
2025-06-26 14:20:46 +1100

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-# main.py
-# This is the main entry point for the game.
 import sys
 import os
 
-import time
-from fonctions.display_scene import display_scene
-from fonctions.choix import choix
+from Fonctions.display_scene import display_scene
+from Fonctions.choix import choix
 from story.intro import scene_intro, choix_intro, rep_intro, scene_intro_2
 
 display_scene(scene_intro)
 choix(choix_intro, rep_intro)
-time.sleep(1)
 display_scene(scene_intro_2)
